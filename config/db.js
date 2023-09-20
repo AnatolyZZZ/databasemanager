0500531c083216bbbb3a567768e57c70c2798243
Anatoly Zykov <a.zykov@school-olymp.ru>
2023-09-20 14:39:53 +0300

    added || to process.env

--- config/db.js
+++ config/db.js
@@ -6,10 +6,10 @@ dotenv.config();
 export const db = knex({
     client: 'pg',
     connection: {
-      host : process.env.DB_HOST,
-      port : process.env.DB_PORT,
-      user : process.env.DB_USER,
-      password : process.env.DB_PASS,
-      database : process.env.DB_NAME
+      host : process.env.DB_HOST || 'manny.db.elephantsql.com',
+      port : process.env.DB_PORT || 5432,
+      user : process.env.DB_USER || 'usftjyce',
+      password : process.env.DB_PASS || 'rFaRvxw2rPAARDLqVtW9H2hzFSbZEWYv',
+      database : process.env.DB_NAME || 'usftjyce'
     }
   })
