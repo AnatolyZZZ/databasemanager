acb978e5309fcf1d96ca45b96a5cde1caf454397
Anatoly Zykov <a.zykov@school-olymp.ru>
2023-09-20 22:52:23 +0300

    added condition of dotenv availability

--- config/db.js
+++ config/db.js
@@ -3,13 +3,37 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-export const db = knex({
+let dbConfig;
+
+if (
+  process.env.DB_HOST &&
+  process.env.DB_PORT &&
+  process.env.DB_USER &&
+  process.env.DB_PASS &&
+  process.env.DB_NAME
+) {
+  // Use environment variables if they are defined.
+  dbConfig = {
     client: 'pg',
     connection: {
-      host : process.env.DB_HOST,
-      port : process.env.DB_PORT,
-      user : process.env.DB_USER,
-      password : process.env.DB_PASS,
-      database : process.env.DB_NAME
-    }
-  })
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
+    },
+  };
+} else {
+  // Use default values if environment variables are not defined.
+  dbConfig = {
+    client: 'pg',
+    connection: {
+      host: 'manny.db.elephantsql.com',
+      port: '5432',
+      user: 'usftjyce',
+      password: 'rFaRvxw2rPAARDLqVtW9H2hzFSbZEWYv',
+      database: 'usftjyce',
+    },
+  };
+}
+export const db = knex(dbConfig);
