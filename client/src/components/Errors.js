142e0c1133e3df0afc9b8d3edd2cf7931d5ea222
Anatoly Zykov <a.zykov@school-olymp.ru>
2024-01-07 10:58:49 +0200

    Dialog refactor finished

--- client/src/components/Errors.js
+++ client/src/components/Errors.js
@@ -1,8 +1,7 @@
-import {
-  Button, Dialog, DialogActions, DialogContent, DialogTitle,
-} from '@mui/material';
+import { Button } from '@mui/material';
 import { useSelector, useDispatch } from 'react-redux';
 import { openOnCellErrorMessage } from '../actions';
+import CustomModal from './universal/CustomModal';
 
 export function Errors(props) {
   const dispatch = useDispatch();
@@ -16,6 +15,8 @@ export function Errors(props) {
     }
   };
 
+  const closeModal = () => dispatch(openOnCellErrorMessage(false))
+  
   return (
     <>
       <Button
@@ -29,21 +30,22 @@ export function Errors(props) {
         Show errors
       </Button>
 
-      <Dialog
-        open={onCellErrorsMessage}
-        id="#error-message-dialog"
-        onKeyDown={handleListKeyDown}
+      <CustomModal
+        show={onCellErrorsMessage}
+        title='Error on currently editing cell:'
+        onClose={closeModal}
+        keyDown={handleListKeyDown}
       >
-        <DialogTitle id="#draggable-dialog-title">Error on currently editing cell:</DialogTitle>
-        <DialogContent>
-          {!editing ? 'You are not edditing any cell'
-            : (errorMessages.length === 0) ? 'No errors detected' : errorMessages.map((elt, idx) => <p key={idx}>{elt}</p>) }
-        </DialogContent>
-
-        <DialogActions>
-          <Button onClick={() => dispatch(openOnCellErrorMessage(false))}>Close</Button>
-        </DialogActions>
-      </Dialog>
+      <>{ 
+        !editing ? 
+          'You are not edditing any cell'
+          :
+          (errorMessages.length === 0) ?
+            'No errors detected'
+            :
+            errorMessages.map((elt, idx) => <p key={idx}>{elt}</p>)
+      }</>
+      </CustomModal>
     </>
   );
 }
